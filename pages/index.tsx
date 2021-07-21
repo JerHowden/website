84b88a75e79c353f8d07934a61377674f1314413
JerHowden <jeremiahhowden@gmail.com>
2021-07-21 02:08:34 -0500

    logo and manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Navigation from '../components/Navigation'
+
 export default function Home() {
 	return (
 		<div>
 			<Head>
 				<title>{'Jeremiah\'s Website'}</title>
 				<meta name="description" content="This is my website, built with nextjs, typescript, and tailwind, deployed with vercel, and includes a collection of different facets of my life." />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/jh.svg" />
 			</Head>
+
+			<Navigation />
 
 			<main>
 				yo
