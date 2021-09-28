4e9c43eb23d74f18e37bf38d72ec8f132fd92dfb
JerHowden <jeremiahhowden@gmail.com>
2021-09-28 13:57:37 -0500

    sitemap and robots for seo

--- pages/todo/index.tsx
+++ pages/todo/index.tsx
@@ -6,11 +6,12 @@ function Todo(): JSX.Element {
         <div className='flex flex-col p-8 border gap-y-8'>
             <h1 className='text-4xl italic font-extrabold'>{'// TODO:'}</h1>
             <ul className='flex flex-col ml-4 text-2xl list-disc gap-y-6'>
-                <li>Modal for Contact Me in Footer</li>
-                <li>Update Resume for resume.pdf</li>
+                <li className='text-gray-500'>Modal for Contact Me in Footer</li>
+                <li className='line-through'>Update Resume for resume.pdf</li>
                 <li>Write blog post about Spotify api</li>
                 <li>Blog wrapper on /blog</li>
                 <li>Guestbook</li>
+                <li className='line-through'>Sitemap</li>
             </ul>
         </div>
     )
