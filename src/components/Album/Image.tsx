2c0b962171c06e8eafc067a2667bafdcb2298e01
Jeremiah Howden <jeremiahhowden@gmail.com>
2024-07-26 16:39:00 -0500

    swiper: added

---
+++ src/components/Album/Image.tsx
@@ -0,0 +1,11 @@
+import { Box, Stack } from '@mui/material'
+
+export function Image() {
+  return (
+    <Stack direction="column">
+      <Box>Image 1</Box>
+      <Box>Image 2</Box>
+      <Box>Image 3</Box>
+    </Stack>
+  )
+}
